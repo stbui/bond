0836c35f09099714cf4bc1b121670f3ca70b58f3
stbui <stbui@stbui.com>
2022-01-11 15:49:06 +0800

    feat: wip

--- jsl.sh
+++ jsl.sh
@@ -1,4 +1,5 @@
-curl -o json.json 'https://www.jisilu.cn/webapi/cb/list_new/' \
+
+curl -o  ./public/date.$(date +'%Y%m%d').json 'https://www.jisilu.cn/webapi/cb/list_new/' \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36' \
   -H 'Referer: https://www.jisilu.cn/web/data/cb/list' \
   -H 'Cookie: kbzw__Session=3rkrj4k87sat5a574gv30g8536; kbz_newcookie=1; kbzw_r_uname=w431106; Hm_lvt_164fe01b1433a19b507595a43bf58262=1637642810,1639136839,1639998407; kbzw__user_login=7Obd08_P1ebax9aX7papkqinqZmcndHV7Ojg6N7bwNOM2aykqJejlK3frZyu0K_ErpOnq9Xel6aSrN-om9-erceXnKTs3Ny_zYylra2eraWog7GOzLjR1Yy90O7i4dXam6yQpoGfxNTbyt7V4-zdpJm15dbbzZfE1NuBv7SU0ZWaq8unwaeKl7jj6M3VuNnbwNLtm6yVrY-qrZOgrLi1wcWhieXV4seWqNza3ueKkKTc6-TW3puvm6SRpaumsJqekqaxlbza0tjU35CsqqqmlKY.; Hm_lpvt_164fe01b1433a19b507595a43bf58262=1640080386' \
